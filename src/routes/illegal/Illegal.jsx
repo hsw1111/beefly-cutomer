647b985a9ed0bf7ca1610d351fccd8cd997c1ba3
杨可可 <yangkeke@mmuu.com>
2018-01-05 14:25:32 +0800

    model

--- src/routes/illegal/Illegal.jsx
+++ src/routes/illegal/Illegal.jsx
@@ -23,30 +23,29 @@ export default class Illegal extends React.Component {
 				{title: '上报编号', data: 'id'},
 				{title: '城市', data: 'cityName'},
 				{title: '上报姓名', data: 'userName'},
-				{title: '上报角色', data: 'reportRole', render: (data) => dtUtils.renderMap(data,reportState)},
+				{title: '上报角色', data: 'reportRole', render: (data) => dtUtils.renderMap(data, reportState)},
 				{title: '手机号', data: 'mobile'},
 				{title: '车辆编号', data: 'bikeCode'},
 				{title: '上报时间', data: 'lastReportTime', render: dtUtils.renderDateTime},
-				{title: '处理进度', data: 'state', render: (data) => dtUtils.renderMap(data,handleType)},
+				{title: '处理进度', data: 'state', render: (data) => dtUtils.renderMap(data, handleType)},
 				{title: '操作', type: 'object', render: this.renderActions},
 			],
-			modalShow: false,
-			inputs: [],
-
 
 			query: {
 				'state': '',
+				'stateText': '',
 				'cityCode': '',
+				'cityName': '全国',
 				'beginDate': '',
 				'endDate': '',
 				'mobile': '',
 				'bikeCode': '',
 				'content': '',
-				'carState': ''
-			},
+				'carState': '',
 
-			showAddRemarkModal: false,
-			showRejectModal: false,
+				'keywordType': 'mobile',
+				'keyword': '',
+			},
 
 		};
 
@@ -67,57 +66,43 @@ export default class Illegal extends React.Component {
 	}
 
 	render() {
-		let {columns, query, modalShow, showAddRemarkModal, showRejectModal} = this.state;
+		let {columns, query} = this.state;
 		return (
 			<Content>
 				<Box theme="query">
 					<Form inline>
-						<Select ref={(e) => this._handleStatus = e} label="处理进度" options={handleType} whole={true}/>
-						<CitySelect ref={(e) => this._citySelect = e} label='城市区域'/>
-						<Select ref={(e) => this._operateStatus = e} label="车辆运营状态" options={operateState}
-								whole={true}/>
-						<Select ref={(e) => this._reportStatus = e} label="上报角色" options={reportState} whole={true}/>
-						<DateRange ref={(e) => this._dateRange = e} label="上报时间"/>
-						<SelectInput ref={(e) => this._selectInput = e} label="模糊搜索" selectOptions={vagueState}/>
+						<Select label="处理进度" model="query.state" options={handleType} whole={true}/>
+						<CitySelect label='城市区域' model="query.cityCode"/>
+						<Select label="车辆运营状态" model="query.carState" options={operateState} whole={true}/>
+						<Select label="上报角色" model="query.content" options={reportState} whole={true}/>
+						<DateRange label="上报时间" model="query.beginDate,query.endDate"/>
+						<SelectInput label="模糊搜索" model="query.keywordType,query.keyword" selectOptions={vagueState}/>
 						<Button icon="search" onClick={this.search.bind(this)}>查询</Button>
 					</Form>
-					<DataTable columns={columns} api={tripProblemApi.page} query={query}/>
+					<DataTable ref={(e) => this._dataTable = e}
+							   columns={columns} api={tripProblemApi.page} query={query}/>
 				</Box>
 
 				<AddRemarkModal ref={(e) => this._addRemarkModal = e}/>
-				<RejectModal show={showRejectModal}/>
+				<RejectModal ref={(e) => this._rejectModal = e}/>
 
 			</Content>
 		)
 	}
 
 	search() {
-		let query = {
-			'state': this._handleStatus.value,
-			'cityCode': this._citySelect.cityCode,
-			'beginDate': this._dateRange.startDate,
-			'endDate': this._dateRange.endDate,
-			'mobile': '',
-			'bikeCode': '',
-			'content': this._reportStatus.value,
-			'carState': this._operateStatus.value
-		};
+		let {query} = this.state;
 
-		if (this._selectInput.selectValue == 'mobile') {
-			query.mobile = this._selectInput.inputValue;
-			query.bikeCode = ''
-		} else {
-			query.mobile = '';
-			query.bikeCode = this._selectInput.inputValue
-		}
+		query.mobile = query.bikeCode = '';
+		query[query.keywordType] = query.keyword;
 
-		this.setState({query})
+		this._dataTable.search(query);
 	}
 
 	// 查看详情
 	details(id) {
 		utils.addTab({
-			name: '违停上报详情-'+id,
+			name: '违停上报详情-' + id,
 			path: '/illegal/details',
 			params: {
 				id
@@ -127,16 +112,12 @@ export default class Illegal extends React.Component {
 
 	// 添加备注
 	addRemark(id) {
-		this._addRemarkModal.show({
-			id
-		});
+		this._addRemarkModal.show({id});
 	}
 
 	// 驳回处理
-	reject() {
-		this.setState({
-			showRejectModal: true
-		})
+	reject(id) {
+		this._rejectModal.show({id});
 	}
 
 	// 确认处理

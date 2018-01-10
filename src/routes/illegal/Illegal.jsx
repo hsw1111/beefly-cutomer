b4520ec34d067be35012b4717c0c189a8ce2fe20
杨可可 <yangkeke@mmuu.com>
2018-01-10 17:59:02 +0800

    cityselect

--- src/routes/illegal/Illegal.jsx
+++ src/routes/illegal/Illegal.jsx
@@ -55,13 +55,13 @@ export default class Illegal extends React.Component {
 
 	renderActions(data, type, row) {
 
-		if(row.state == 5){
+		if (row.state == 5) {
 			let actions = [
 				{text: '查看详情', icon: 'search', onclick: `beefly.details('${row.id}')`},
 				{text: '添加备注', icon: 'user-plus', onclick: `beefly.addRemark('${row.id}')`},
 			];
 			return dtUtils.renderActions(actions, 'dropdown')
-		}else {
+		} else {
 			let actions = [
 				{text: '查看详情', icon: 'search', onclick: `beefly.details('${row.id}')`},
 				{text: '添加备注', icon: 'user-plus', onclick: `beefly.addRemark('${row.id}')`},

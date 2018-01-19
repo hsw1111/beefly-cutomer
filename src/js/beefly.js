b7663721ae8e71f468895ccf58c70986a9527a4b
star-peng <18341533741@163.com>
2018-01-19 16:38:25 +0800

    up   修改

--- src/js/beefly.js
+++ src/js/beefly.js
@@ -1,6 +1,18 @@
-import {utils} from 'beefly-common';
 
-let beefly = utils;
+let beefly = {
+
+	DateMinus(date1,date2){
+		let type1 =new Date( date1);
+		let type2 =new Date( date2);
+		let s1 = type1.getTime();
+		let s2 = type2.getTime();
+		let total = (s2 - s1)/1000;
+
+		let day = total / (24*60*60);//计算整数天数
+		return day
+	}
+
+};
 
 window.beefly = beefly;
 export default beefly;

f725de40aad7d3568603aed0257fb28c0c41c687
杨可可 <yangkeke@mmuu.com>
2018-01-26 10:25:34 +0800

    update

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -4,13 +4,12 @@ import {Content} from 'beefly-common';
 export default class Home extends React.Component {
 
 	render() {
+		let {company, platform} = window.config;
 		return (
 			<Content>
 				<div className="row margin-bottom">
 					<div className="text-center">
-
-						<h1>欢迎登录蜜蜂出行客服管理平台</h1>
-
+						<h1>欢迎登录{company}{platform}</h1>
 					</div>
 				</div>
 			</Content>
